43cb0d046fe28245e03bcc883f5e788c8d1ed6ca
ruben,boada <rubenboadana@gmail.com>
2023-05-16 16:41:53 +0200

    Remove unused code

--- src/main/resources/archetype-resources/src/main/java/product/domain/dto/ProductDTO.java
+++ src/main/resources/archetype-resources/src/main/java/product/domain/dto/ProductDTO.java
@@ -3,7 +3,6 @@
 #set( $symbol_escape = '\' )
 package ${package}.product.domain.dto;
 
-import ${package}.product.domain.exceptions.NotEnoughStockException;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,16 +30,6 @@ public class ProductDTO {
 
     public int getQuantity() {
         return quantity.getValue();
-    }
-
-    public boolean hasEnoughStock(int requestedQuantity) {
-        return getQuantity() >= requestedQuantity;
-    }
-
-    public void checkIfHasEnoughStock(int requestedQuantity) {
-        if (!hasEnoughStock(requestedQuantity)) {
-            throw new NotEnoughStockException();
-        }
     }
 
 }
